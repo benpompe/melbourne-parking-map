1a9d6fc085323ba1e3a401ec5f7575fe1134ea40
benpompe <benpompe2@gmail.com>
2026-07-19 19:58:34 +1000

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,9 @@
+const map = L.map('map').setView([-37.8136, 144.9631], 14);
 
+L.tileLayer(
+  'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+  {
+    attribution: '&copy; OpenStreetMap contributors & CARTO',
+    maxZoom: 20
+  }
+).addTo(map);
